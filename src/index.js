21bffab1b043afa38338bbb69b9907f53d54a252
Roman Nikitenko <Lomkaaa@ukr.net>
2023-02-24 09:16:52 +0200

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Posts } from "./components/Posts";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -9,12 +9,12 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store} >
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Posts" element={<Posts />} />
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
